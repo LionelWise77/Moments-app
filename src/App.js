653efffdae67b7d758f3a8461488bb1c662e5fb7
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-11-19 21:11:22 +0000

    home page and sign in/up links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import styles from "../src/App.module.css";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>Page Not Found</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
